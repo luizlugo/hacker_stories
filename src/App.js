fe9689568d38439b834ad4c850e5f05fdf0b352f
luislugoshockoe <luis.lugo@shockoe.com>
2023-04-08 13:19:02 -0600

    Adding state to component

--- src/App.js
+++ src/App.js
@@ -1,85 +1,103 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
 
-const list = [
-  {
-    title: "React",
-    url: "https://reactjs.org/",
-    author: "Jordan Walke",
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: "Redux",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: "Redux 2",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 2,
-  },
-  {
-    title: "Redux 3",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 3,
-  },
-  {
-    title: "Redux 4",
-    url: "https://redux.js.org/",
-    author: "Dan Abramov, Andrew Clark",
-    num_comments: 2,
-    points: 5,
-    objectID: 4,
-  },
-];
-
 const App = () => {
+  console.log("App renders");
+
+  const stories = [
+    {
+      title: "React",
+      url: "https://reactjs.org/",
+      author: "Jordan Walke",
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: "Redux",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: "Redux 2",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 2,
+    },
+    {
+      title: "Redux 3",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 3,
+    },
+    {
+      title: "Redux 4",
+      url: "https://redux.js.org/",
+      author: "Dan Abramov, Andrew Clark",
+      num_comments: 2,
+      points: 5,
+      objectID: 4,
+    },
+  ];
+
   return (
     <div>
       <h1>My Hacker Stories</h1>
       <Search />
       <hr />
-      <List />
+      <List list={stories} />
     </div>
   );
 };
 
 const Search = () => {
+  console.log("Search renders");
+  const [searchText, setSearchText] = React.useState("");
+
+  const onChange = (event) => {
+    setSearchText(event.target.value);
+  };
+
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={onChange} />
+
+      <p>
+        Searching for <strong>{searchText}</strong>
+      </p>
     </div>
   );
 };
 
-const List = () => {
+const List = ({ list }) => {
+  console.log("List renders");
   return (
     <ul>
       {list.map((item) => (
-        <li key={item.objectID}>
-          <span>
-            <a href={item.url}>{item.title}</a>
-          </span>
-          <span> {item.author}</span>
-          <span> {item.num_comments}</span>
-          <span> {item.points}</span>
-        </li>
+        <ListItem item={item} key={item.objectID} />
       ))}
     </ul>
   );
 };
+
+const ListItem = ({ item }) => (
+  <li>
+    <span>
+      <a href={item.url}>{item.title}</a>
+    </span>
+    <span> {item.author}</span>
+    <span> {item.num_comments}</span>
+    <span> {item.points}</span>
+  </li>
+);
 
 class Developer {
   constructor(firstName, lastName) {
